bf3f46588dd926c0b6ae3f19aab405e244c1cdd1
Austin <austinbreathesair@gmail.com>
2019-07-18 15:30:38 +0200

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { ErrorComponent } from './error/error.component';
+import { SuccessComponent } from './success/success.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    ErrorComponent,
+    SuccessComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
